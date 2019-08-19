59ec19c7d9e6d07e6b08d15e445709da6f3d7034
Sebastian Guzman <sebasgiustra@gmail.com>
2019-08-19 14:51:25 -0300

    Creada vista individual

--- resources/views/viewdata.blade.php
+++ resources/views/viewdata.blade.php
@@ -74,8 +74,9 @@
                   @foreach($books as $book)
                   <tr>
                     <td> <img src="{{ $book->portada }}" width=50px; alt=""></td>
+
                     <td class="vertical">{{ $book->ean }}</td>
-                    <td class="vertical">{{ $book->titulo }}</td>
+                    <td class="vertical"><a href="#modal-{{$book->id}}" data-toggle="modal" data-target="#modal-{{$book->id}}">{{ $book->titulo }}</a></td>
                     <td>{{ $book->apellido_autor }} {{ $book->nombre_autor }}</td>
 
                     <td>{{ $book->editorial }}</td>
@@ -96,8 +97,6 @@
                   @endforeach
                   </tbody>
 
-
-
                 </table>
 
               </div>
@@ -114,7 +113,78 @@
         <!-- /.col -->
       </div>
 
+      @foreach($books as $book)
+      <div class="modal fade" id="modal-{{$book->id}}">
+      <div class="modal-dialog">
+          <div class="modal-content">
+          <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span></button>
+              <h4 class="modal-title">{{ $book->titulo }} </h4>
+          </div>
+          <div class="modal-body">
+              <p><table class="table no-margin">
+                      <tr>
+                        <th colspan="2" rowspan="8"><img src="{{ $book->portada }}" width=150px; alt=""></th>
+                        <th></th>
+                        <th></th>
+                      </tr>
+                      <tr>
+                        <td><span class="link-black text-sm">ISBN: </span>{{ $book->isbn13 }}</td>
+                        <td><span class="link-black text-sm">EAN: </span>{{ $book->ean }}</td>
+                      </tr>
+                      <tr>
+                        <td><span class="link-black text-sm">Ilustrador: </span>{{ $book->ilustrador }}</td>
+                        <td><span class="link-black text-sm">Edición: </span>{{ $book->fecha_publicacion }}</td>
+                      </tr>
+                      <tr>
+                        <td><span class="link-black text-sm">Traductor: </span>{{ $book->traductor }}</td>
+                        <td><span class="link-black text-sm">Editorial: </span><strong>{{ $book->editorial }}</strong></td>
+                      </tr>
+                      <tr>
+                        <td><span class="link-black text-sm">Colección: </span>{{ $book->coleccion }}</td>
+                        <td><span class="link-black text-sm">Categoría: </span>{{ $book->categoria }}</td>
+                      </tr>
+                      <tr>
+                        <td><span class="link-black text-sm">Tipo: </span>{{ $book->tipo }}</td>
+                        <td><span class="link-black text-sm">Género: </span>{{ $book->genero }}</td>
+                      </tr>
+                      <tr>
+                        <td colspan="2"><span class="link-black text-sm">Título: </span>{{ $book->titulo }}</td>
+                      </tr>
+                      <tr>
+                        <td colspan="2"><span class="link-black text-sm">Autor: </span>{{ $book->apellido_autor }} {{ $book->nombre_autor }}</td>
+                      </tr>
+                      <tr>
+                        <td colspan="4"><span class="link-black text-sm">Sinopsis: </span><br>
+                            {{ $book->sinopsis }}</td>
+                      </tr>
+                      <tr>
+                        <td><span class="link-black text-sm">Edición: </span>{{ $book->edicion }}</td>
+                        <td><span class="link-black text-sm">Páginas: </span>{{ $book->paginas }}</td>
+                        <td><span class="link-black text-sm">Medidas: </span>{{ $book->medidas }}</td>
+                        <td><span class="link-black text-sm">Agotado? </span>{{ $book->agotado }}</td>
+                      </tr>
+                      <tr>
+                        <td><span class="link-black text-sm">Precio Venta: $</span>{{ $book->pvp }}</td>
+                        <td><span class="link-black text-sm">Idioma: </span>{{ $book->idioma }}</td>
+                        <td><span class="link-black text-sm">Peso: </span>{{ $book->peso }}</td>
+                        <td><span class="link-black text-sm">Activo? </span> {{ $book->activo }}</td>
+                      </tr>
+                    </table>
+                  </p>
+          </div>
+          <div class="modal-footer">
+              <button type="button" class="btn btn-primary" data-dismiss="modal">Cerrar</button>
 
+          </div>
+          </div>
+          <!-- /.modal-content -->
+      </div>
+      <!-- /.modal-dialog -->
+      </div>
+      <!-- /.modal -->
+@endforeach
       </section>
     <!-- /.content -->
   </div>
